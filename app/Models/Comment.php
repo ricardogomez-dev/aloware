fbfbfb23615cf01cf1687199b1fa549ee095aba2
ricardogomez-dev <Ricardogomez95@outlook.com>
2022-05-06 20:33:35 -0500

    Add comment timestamp

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,6 +12,8 @@ class Comment extends Model
 
     protected $guarded = [];
 
+    protected $appends = ['timestamp'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -19,5 +22,10 @@ class Comment extends Model
     public function nested_comments()
     {
         return $this->hasMany(NestedComment::class);
+    }
+
+    public function getTimestampAttribute()
+    {
+        return Carbon::parse($this->created_at)->diffForHumans();
     }
 }
--- resources/js/Components/Comment.vue
+++ resources/js/Components/Comment.vue
@@ -4,6 +4,7 @@
 			<img :src="`https://ui-avatars.com/api/?name=${comment.user.name}&color=80C1E5&background=F1FAFF`" class="rounded-full w-12 h-12">
 			<h3 class="text-gray-700 text-lg font-semibold ml-5">
 				{{ comment.user.name }}
+				<span class="text-gray-400 text-sm ml-2">{{ comment.timestamp }}</span>
 			</h3>
 		</div>
 		<p class="px-5 mt-3 text-gray-500 mb-5">
@@ -13,7 +14,7 @@
 			</span>
 		</p>
 		<form v-if="replyComment" @submit.prevent="createComment">
-			<input id="comment_input" v-model="form.comment" type="text" class="w-full pl-5 pr-10 py-3 border border-gray-200 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm transition-colors" placeholder="Write a new comment...">
+			<input id="comment_input" v-model="form.comment" type="text" class="w-full pl-5 pr-10 py-3 border border-gray-200 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm transition-colors" placeholder="Reply comment...">
 		</form>
 		<div v-for="nested_comment in nested_comments">
 			<NestedComment :comment="nested_comment" />
@@ -47,6 +48,7 @@
 					axios.post(route('nested_comments.store'), this.form)
 						.then(res => {
 							this.form.comment = ''
+							this.replyComment = false
 							this.nested_comments.push(res.data) 
 						})
 				}
